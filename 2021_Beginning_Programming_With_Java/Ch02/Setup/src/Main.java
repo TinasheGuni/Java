7f145219c8dc20c2e6ec4fe256bf9e38b87c8fb1
Tynash <tinashe.gunin@gmail.com>
2022-11-17 12:16:28 +0200

    2022_11_17 - HP Road To Mastery

--- 2021_Beginning_Programming_With_Java/Ch02/Setup/src/Main.java
+++ 2021_Beginning_Programming_With_Java/Ch02/Setup/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The Winter soilder is here motherfuckers");
+        System.out.println("The Winter soldier is here motherfuckers");
     }
 }
