e8c0906d9bfbbe1f5eed951f0dbcd86af61e2017
Tynash <tinashegunin@gmail.com>
2022-12-19 15:38:34 +0200

    2022_12_19 - Acer Road to Mastery

---
+++ 2021_Beginning_Programming_With_Java/Ch03/Mortgage/src/Mortgage.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+
+public class Mortgage {
+
+    public  static void main(string args[] thows IOException) {
+
+
+    }
+
+}
